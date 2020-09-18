c5ca8d086c4be7ab844c4c981b01574e47722a23
Oladimeji Vincent <60340075+vincey101@users.noreply.github.com>
2020-09-18 01:59:51 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+var bcrypt = require("bcryptjs");
+var cors = require("cors");
+const knex = require("knex");
+// const { response } = require("express");
+
+const register = require("./controllers/register");
+const signin = require("./controllers/signin");
+const profile = require("./controllers/profile");
+const image = require("./controllers/image");
+
+const db = knex({
+  client: "pg",
+  connection: {
+    host: "127.0.0.1",
+    user: "postgres",
+    password: "breitling",
+    database: "smart-brain",
+  },
+});
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send(database.users);
+});
+app.post("/signin", (req, res) => { signin.handleSignin(req, res, db, bcrypt) })
+app.post("/register", (req, res) => { register.handleRegister(req, res, db) })
+app.get("/profile/:id", (req, res) => { profile.handleProfileGet(req, res, db) });
+app.put("/image", (req, res) => { image.handleImage(req, res, db) });
+app.post("/imageurl", (req, res) => { image.handleApiCall(req, res) });
+
+
+app.listen(4000, () => {
+  console.log("app is running on port 4000");
+});
