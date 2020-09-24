30741aff7cb928907b3dc054f8835e680b0f0903
Oladimeji Vincent <60340075+vincey101@users.noreply.github.com>
2020-09-24 19:41:55 +0100

    ediiting register.js

--- controllers/register.js
+++ controllers/register.js
@@ -3,7 +3,7 @@ const handleRegister = (req, res, db, bcrypt) => {
     if (!email || !name || !password) {
         return res.status(400).json('incorrect form submission')
     }
-    const hash = bcrypt.hashSync(password, 10);
+    const hash = bcrypt.hashSync(password);
     db.transaction((trx) => {
         trx
             .insert({
