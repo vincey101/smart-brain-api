f20e6497f00231530c1e7f8ab6eab5423da53abe
Oladimeji Vincent <60340075+vincey101@users.noreply.github.com>
2020-09-24 19:07:45 +0100

    updating database

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const image = require("./controllers/image");
 const db = knex({
   client: "pg",
   connection: {
-    host: "127.0.0.1",
+    host: "postgresql-animated-25665",
     user: "postgres",
     password: "breitling",
     database: "smart-brain",
