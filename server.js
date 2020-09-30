6af71684e885366df66e074f80c7960b4f2b700a
Oladimeji Vincent <60340075+vincey101@users.noreply.github.com>
2020-09-30 18:33:40 +0100

    updating register.js

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.get("/", (req, res) => {
   res.send('it is working');
 });
 app.post("/signin", (req, res) => { signin.handleSignin(req, res, db, bcrypt) })
-app.post("/register", (req, res) => { register.handleRegister(req, res, db) })
+app.post("/register", (req, res) => { register.handleRegister(req, res, db,bcrypt) })
 app.get("/profile/:id", (req, res) => { profile.handleProfileGet(req, res, db) });
 app.put("/image", (req, res) => { image.handleImage(req, res, db) });
 app.post("/imageurl", (req, res) => { image.handleApiCall(req, res) });
