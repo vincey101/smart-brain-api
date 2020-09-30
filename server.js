a248c50e9cffbe09bafcee44afcf1e0092096743
Oladimeji Vincent <60340075+vincey101@users.noreply.github.com>
2020-09-30 18:58:50 +0100

    updating database connection

--- server.js
+++ server.js
@@ -13,10 +13,10 @@ const image = require("./controllers/image");
 const db = knex({
   client: "pg",
   connection: {
-    host: "postgresql-animated-25665",
-    user: "postgres",
-    password: "breitling",
-    database: "smart-brain",
+    host: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
+    }
   },
 });
 
@@ -29,7 +29,7 @@ app.get("/", (req, res) => {
   res.send('it is working');
 });
 app.post("/signin", (req, res) => { signin.handleSignin(req, res, db, bcrypt) })
-app.post("/register", (req, res) => { register.handleRegister(req, res, db,bcrypt) })
+app.post("/register", (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 app.get("/profile/:id", (req, res) => { profile.handleProfileGet(req, res, db) });
 app.put("/image", (req, res) => { image.handleImage(req, res, db) });
 app.post("/imageurl", (req, res) => { image.handleApiCall(req, res) });
